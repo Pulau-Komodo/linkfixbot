5bfa0b6ffd35b7b139fc4e115d806a0f78ba1f2c
John Doe <paladinarr+gitlab@gmail.com>
2025-04-11 18:09:51 +0000

    Fix spoiler regex

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use std::sync::LazyLock;
 
 use regex::Regex;
 
-static SPOILERS: LazyLock<Regex> = LazyLock::new(|| Regex::new("||").unwrap());
+static SPOILERS: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\|\|").unwrap());
 
 /// Overly aggressive function detecting any possible presence of spoiler tags.
 pub fn has_spoilers(str: &str) -> bool {
